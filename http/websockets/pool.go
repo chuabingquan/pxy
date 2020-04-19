3cba63dbc8e304a58aaa378981c2d9441f1503f5
Chua Bing Quan <chipneverdies@gmail.com>
2020-04-20 03:15:26 +0800

    Implement livestream websocket registration

--- http/websockets/pool.go
+++ http/websockets/pool.go
@@ -1,16 +1,89 @@
 package websockets
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
 
-// LivestreamUpdates ...
-type LivestreamUpdates struct{}
+	"github.com/gorilla/websocket"
+)
+
+// LivestreamPool ...
+type LivestreamPool struct {
+	connections map[string]*livestreamConnection
+	upgrader    websocket.Upgrader
+	lock        sync.RWMutex
+}
+
+// NewLivestreamPool ...
+func NewLivestreamPool(readBufferSize, writeBufferSize int, subprotocols []string) *LivestreamPool {
+	return &LivestreamPool{
+		connections: make(map[string]*livestreamConnection),
+		upgrader: websocket.Upgrader{
+			ReadBufferSize:  readBufferSize,
+			WriteBufferSize: writeBufferSize,
+			Subprotocols:    subprotocols,
+			CheckOrigin:     func(r *http.Request) bool { return true },
+		},
+		lock: sync.RWMutex{},
+	}
+}
 
 // RegisterStreamer ...
-func (lu *LivestreamUpdates) RegisterStreamer(sessionID, userID, streamKey string, w http.ResponseWriter, r *http.Request) error {
+func (lp *LivestreamPool) RegisterStreamer(sessionID, userID, streamKey string, w http.ResponseWriter, r *http.Request) error {
+	lp.lock.Lock()
+	defer lp.lock.Unlock()
+
+	conn, err := lp.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return fmt.Errorf("Failed to upgrade HTTP connection to a websocket: %w", err)
+	}
+
+	newStreamer := newLivestreamConnection(sessionID, userID, streamKey, conn)
+
+	if _, exists := lp.connections[sessionID]; !exists {
+		lp.connections[sessionID] = newStreamer
+	} else {
+		lp.connections[sessionID].Websocket.Close()
+		lp.connections[sessionID] = newStreamer
+	}
+
+	go func() {
+		for {
+			messageType, payload, err := newStreamer.Websocket.ReadMessage()
+			if err != nil || messageType == websocket.CloseMessage {
+				lp.lock.Lock()
+				defer lp.lock.Unlock()
+
+				newStreamer.Websocket.Close()
+				if existingStreamer, exists := lp.connections[sessionID]; exists {
+					if existingStreamer.ID == newStreamer.ID {
+						delete(lp.connections, sessionID)
+					}
+				}
+				break
+			}
+
+			log.Println(payload)
+		}
+	}()
+
 	return nil
 }
 
 // CloseUpdates ...
-func (lu *LivestreamUpdates) CloseUpdates(sessionID string) error {
+func (lp *LivestreamPool) CloseUpdates(sessionID string) error {
+	lp.lock.Lock()
+	defer lp.lock.Unlock()
+
+	if existingStreamer, exists := lp.connections[sessionID]; exists {
+		existingStreamer.Websocket.Close()
+		delete(lp.connections, sessionID)
+	} else {
+		return errors.New("Session to close livestream for does not exist")
+	}
+
 	return nil
 }
